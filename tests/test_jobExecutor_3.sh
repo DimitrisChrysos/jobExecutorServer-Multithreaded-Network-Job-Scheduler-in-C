2017554614220dc6c7ebd6ab2ad23e11b1bfad27
DimitrisChrysos <sdi2100275@di.uoa.gr>
2024-06-03 19:01:02 +0300

    I think test3 works

--- tests/test_jobExecutor_3.sh
+++ tests/test_jobExecutor_3.sh
@@ -1,2 +1,2 @@
-./jobCommander issueJob ls -l /usr/bin/* /usr/local/bin/* /bin/* /sbin/* /opt/* /etc/* /usr/sbin/*
-./jobCommander exit
+./bin/jobCommander linux01.di.uoa.gr 2035 issueJob ls -l /usr/bin/* /usr/local/bin/* /bin/* /sbin/* /opt/* /etc/* /usr/sbin/*
+./bin/jobCommander linux01.di.uoa.gr 2035 exit
